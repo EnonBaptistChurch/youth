1daf5c1f0d491ee1c45a81524003d50e5a0e90a5
David <davidmitchellfootball@googlemail.com>
2026-01-10 19:59:10 +0000

    trial change for page load

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",   // static export
-  basePath: "/",      // no extra prefix
-  assetPrefix: "/",   // load assets from repo root
+  basePath: "",      // no extra prefix
+  assetPrefix: "",   // load assets from repo root
 };
 
 export default nextConfig;
