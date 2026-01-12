d438b4fa670d88735c4af3c9600a63231ea4dc3c
David <davidmitchellfootball@googlemail.com>
2026-01-12 10:21:04 +0000

    change base path for dev

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
   output: 'export',
-  basePath: '/youth',  // ← ADD THIS!
+  basePath: isProd ? '/youth' : '',
   reactStrictMode: true,
   images: {
     unoptimized: true,
