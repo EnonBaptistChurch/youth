4c7ec4f59b17a4a0a5634e92ebd143f37ae5d4c8
David <davidmitchellfootball@googlemail.com>
2026-01-14 11:58:01 +0000

    change header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <header className="bg-blue-950 text-white p-1">
+    <header className="bg-blue-950 text-white">
       <a href="/">
         <h1 className="text-3xl font-bold">
           Enon Baptist Church Tools
