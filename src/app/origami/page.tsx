e8205178a4aaf5a7fc3751d18fc53ac56ac29369
David <davidmitchellfootball@googlemail.com>
2026-01-24 14:56:57 +0000

    rm basepath for origami page as going to route regardless with I think works?

--- src/app/origami/page.tsx
+++ src/app/origami/page.tsx
@@ -13,12 +13,10 @@ export default function Page() {
     .sort(); // sort alphabetically
   
   const images = files.map((f) => basePath + `/origami/images/${f}`);
-
-  const linkHref = basePath || '/'; 
   
   return (
     <main style={{ maxWidth: 800, margin: "0 auto", padding: 16 }}>
-      <Link className="pb-4 inline-block" href={linkHref}>
+      <Link className="pb-4 inline-block" href="/">
       <svg className="inline-block mr-2" width="24px" height="24px" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg" data-iconid="390866" data-svgname="Back">
       <path fill="currentColor" d="M224 480h640a32 32 0 110 64H224a32 32 0 010-64z"/>
       <path fill="currentColor" d="M237.248 512l265.408 265.344a32 32 0 01-45.312 45.312l-288-288a32 32 0 010-45.312l288-288a32 32 0 1145.312 45.312L237.248 512z"/>
